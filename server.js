01cd555960bbef3f8dfdf33d647193d6c211dd3b
outum1500 <outum1500@gmail.com>
2019-11-11 18:00:18 +0900

    first

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const ejs = require('ejs');
+const path = require('path');
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+var router = express.Router();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+app.get('/', (req, res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+})
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
